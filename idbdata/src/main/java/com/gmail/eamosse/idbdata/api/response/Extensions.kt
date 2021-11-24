04cb4a62cd85b755a74a180376c98b96ff4ff67e
sheeeeesh <61969570+sheeeeesh@users.noreply.github.com>
2021-11-24 09:54:06 +0100

    commit axel partie 2 terminée

---
+++ idbdata/src/main/java/com/gmail/eamosse/idbdata/api/response/Extensions.kt
@@ -0,0 +1,26 @@
+package com.gmail.eamosse.idbdata.api.response
+
+import com.gmail.eamosse.idbdata.utils.Result
+import java.io.IOException
+
+internal suspend fun <T : Any> safeCall(execute: suspend () -> Result<T>): Result<T> {
+    return try {
+        execute()
+    } catch (e: Exception) {
+        if (e is IOException) {
+            Result.Error(
+                exception = NetworkException(),
+                message = "Problème d'accès au réseau",
+                code = -1
+            )
+        } else {
+            Result.Error(
+                exception = e,
+                message = e.message ?: "No message",
+                code = -1
+            )
+        }
+    }
+}
+
+class NetworkException : Throwable()
