6d3dd21c53543d9bf0f79cd8a57447a66b96b022
sheeeeesh <61969570+sheeeeesh@users.noreply.github.com>
2021-12-15 10:27:42 +0100

    fragment about us

--- app/src/main/java/com/gmail/eamosse/imdb/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = ""
+        value = "Qui sommes nous ?"
     }
     val text: LiveData<String> = _text
 }
