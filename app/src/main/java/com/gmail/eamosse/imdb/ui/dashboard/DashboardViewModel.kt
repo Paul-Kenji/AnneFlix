b8bcbd978a0d132dc6548df49ff1e1f8acd0d49b
sheeeeesh <61969570+sheeeeesh@users.noreply.github.com>
2022-01-14 19:21:23 +0100

    fragment dashboard

--- app/src/main/java/com/gmail/eamosse/imdb/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/dashboard/DashboardViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
+        value = "Now on Anneflix"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/gmail/eamosse/imdb/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Qui sommes nous ?"
+        value = "Who are we ?"
     }
     val text: LiveData<String> = _text
 }
